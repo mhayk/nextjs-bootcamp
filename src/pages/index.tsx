becf080dfdbfa2080edd48cece204c48df8aedac
Mhayk Whandson <hi@mhayk.com>
2020-10-25 21:39:24 +0000

    Server side rendering.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from 'next';
 import { useEffect, useState } from 'react';
 import { Title } from '../styles/pages/Home'
 
@@ -6,16 +7,20 @@ interface IProduct {
   title: string;
 }
 
-export default function Home() {
-  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
 
-  useEffect(() => {
-    fetch('http://localhost:3333/recommended').then(response => {
-      response.json().then(data => {
-        setRecommendedProducts(data);
-      })
-    })
-  }, [])
+export default function Home({recommendedProducts}: HomeProps ) {
+  // const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/recommended').then(response => {
+  //     response.json().then(data => {
+  //       setRecommendedProducts(data);
+  //     })
+  //   })
+  // }, [])
 
   return (
     <div>
@@ -32,4 +37,15 @@ export default function Home() {
       </section>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
 }
